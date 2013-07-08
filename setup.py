3ecdf9baabe9e3a03e19ff23e8b7a2c67059a4f3
Adrian Altenhoff <adrian.altenhoff@inf.ethz.ch>
2013-07-08 16:39:53 +0200

    finished moving to other repository
    added python package structure

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+name = 'familyanalyzer'
+
+setup(
+    name=name,
+    version='0.1.0',
+    author='Adrian Altenhoff',
+    author_email='adrian.altenhoff@inf.ethz.ch',
+    description='todoc',
+    packages=find_packages(),
+    install_requires=[None],
+)
+
