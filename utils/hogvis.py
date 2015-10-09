a30257ab95241fc4cab5566f10cdb62f47ef98b1
Adrian Altenhoff <adrian.altenhoff@inf.ethz.ch>
2015-10-09 14:07:00 +0200

    working prototyp?
    Former-commit-id: ea32c7415e2b5f5a11993b6724856957e301241a

--- utils/hogvis.py
+++ utils/hogvis.py
@@ -1,8 +1,15 @@
 import collections
 import io
+import os
 import unittest
+from unittest.mock import MagicMock
 import familyanalyzer.familyanalyzer as fa
 import json
+import itertools
+import tempfile
+import shutil
+from string import Template
+import re
 
 __author__ = 'adriaal'
 
@@ -16,6 +23,7 @@ class OGLevelMapper(object):
                 self.levels[lev].append(og)
                 pos = len(self.levels[lev]) - 1
                 try:
+                    assert og.get('og') is not None
                     pos_before = self.id2pos[og.get('og')]
                     if pos != pos_before:
                         raise HOGError(
@@ -33,6 +41,9 @@ class OGLevelMapper(object):
 class HOGVisExtractor(object):
     def __init__(self, fname):
         self.parser = fa.OrthoXMLParser(fname)
+        anno = fa.GroupAnnotator(self.parser)
+        anno.annotateDoc()
+        self.re_non_char = re.compile(r'\W')  # matches non-standard chars (not A-Za-z0-9 or _)
 
     def get_per_species_structure(self, fam):
         genes_per_species = self.genenodes_per_species(fam)
@@ -47,9 +58,13 @@ class HOGVisExtractor(object):
                 while self.parser.is_evolutionary_node(parent):
                     if self.parser.is_ortholog_group(parent):
                         for lev in fa.OrthoXMLQuery.getLevels(parent):
+                            lev_repr = lev
+                            if self.re_non_char.search(lev):
+                                lev_repr = '"{}"'.format(lev)
                             if lev not in cur_map:
-                                cur_map[lev] = [[] for _ in range(ogs_mapper.nr_subhogs_on_level(lev))]
-                            cur_map[lev][ogs_mapper.index(parent)].append(int(gene.get('id')))
+                                cur_map[lev_repr] = [[] for _ in range(ogs_mapper.nr_subhogs_on_level(lev))]
+                            if lev != org:
+                                cur_map[lev_repr][ogs_mapper.index(parent)].append(int(gene.get('id')))
                     parent = parent.getparent()
         return per_species
 
@@ -67,9 +82,114 @@ class HOGVisExtractor(object):
             genes[sp].append(gref)
         return genes
 
+    def iter_families(self):
+        for fam in self.parser.getToplevelGroups():
+            topology = self.get_topology(fam)
+            per_species = self.get_per_species_structure(fam)
+            xrefs = self.get_xrefs(fam)
+            yield topology, per_species, xrefs, int(fam.get('id'))
+
+    def get_topology(self, fam):
+        taxonomy = PerFamilyTopology(self.parser, fam)
+        anno = fa.GroupAnnotator(self.parser)
+        anno.dupCnt = list()
+        anno._annotateGroupR(fam, fam.get('id'))
+        anno.tax = taxonomy
+        anno._addTaxRangeR(fam, noUpwardLevels=True)
+        return taxonomy.newick()
+
+    def get_xrefs(self, fam):
+        geneRefs = fa.OrthoXMLQuery.getSubNodes("geneRef", fam)
+        xrefs = {}
+        for gene in geneRefs:
+            gid = gene.get('id')
+            xrefs[int(gid)] = dict(fa.OrthoXMLQuery.getGeneFromId(gid, self.parser.root).attrib)
+        return xrefs
+
+
+class PerFamilyTopology(fa.TaxRangeOrthoXMLTaxonomy):
+    """A Taxonomy Extracter from the orthoxml, but instead of inferring a global
+    taxonomy, do it for each family. This way we can avoid having many levels
+    where nothing happens in the hog, but still ensure that siblings hogs get
+    treated correctly, e.g. no missing intermediate level because of gene loss in
+    a sister lineage."""
+
+    def __init__(self, parser, fam):
+        self.parser = parser
+        self.extractAdjacencies(fam)
+        self.bloat_all()
+        self.extractHierarchy()
+        self.extractDescendentSpecies()
+        self.extractYoungerNodes()
+
+    def extractAdjacencies(self, fam):
+        self.adj = set()
+        self._parseParentChildRelsR(fam)
+        self.nodes = set(itertools.chain(*self.adj))
+
 
 class HOGError(Exception):
     pass
+
+
+class Writer(object):
+    html_template = Template("""<!DOCTYPE html>
+<html>
+<head>
+ <title>$name</title>
+ <!-- D3 -->
+ <script src="http://d3js.org/d3.v3.min.js" charset="utf-8"></script>
+
+ <!-- Underscore -->
+ <script src="http://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.6.0/underscore-min.js"></script>
+
+ <!-- TnT -->
+ <link rel="stylesheet" href="http://cbrg-oma.ethz.ch/static/css/tnt.css" type="text/css" />
+ <script src="http://cbrg-oma.ethz.ch/static/js/tnt.js"></script>
+ <script src="http://cbrg-oma.ethz.ch/static/js/hog.js"></script>
+
+  <h1>HOG Viewer for $name</h1>
+  <div id="hog_tree"></div>
+
+  <script>
+
+    (function () {
+      var tree = tnt.tree.parse_newick('$species_tree');
+        var query;
+        var per_species = $per_species ;
+        var tooltip_data = $xrefs ;
+        var options = {'show_internal_labels': "true",
+                       'oma_info_url_template': ""};
+
+        var viz = tnt.tree_annot();
+        var theme = tnt_theme_tree_hog();
+        theme (viz, document.getElementById("hog_tree"), query, per_species, tree, tooltip_data, options);
+    }) ();
+
+  </script>
+</body>
+</html>""")
+
+    def __init__(self, folder, extractor):
+        if os.path.exists(folder):
+            if not os.path.isdir(folder):
+                raise FileExistsError(folder)
+        else:
+            os.mkdir(folder)
+        self.outdir = folder
+        self.extractor = extractor
+
+    def dump_hogs(self):
+        for famdata in self.extractor.iter_families():
+            newick, per_species, xrefs, fam_id = famdata
+            self.write_hog(fam_id, newick, per_species, xrefs)
+
+    def write_hog(self, fam_id, species_tree, per_species, xrefs):
+        with open(os.path.join(self.outdir, "hog{:06d}.html".format(fam_id)), 'w') as fh:
+            fh.write(self.html_template.safe_substitute({'name': "HOG{:06d}".format(fam_id),
+                                                         'species_tree': species_tree,
+                                                         'xrefs': json.dumps(xrefs),
+                                                         'per_species': json.dumps(per_species)}))
 
 
 class HOGVisExtractorTest(unittest.TestCase):
@@ -157,9 +277,6 @@ Example Notes without meaning
  </groups>
 </orthoXML>""".encode('utf-8'))
         self.hog_extractor = HOGVisExtractor(file)
-        anno = fa.GroupAnnotator(self.hog_extractor.parser)
-        anno.annotateDoc()
-
         self.fam = self.hog_extractor.parser.getToplevelGroups()[0]
         self.map = self.hog_extractor.get_groups_to_level_mapper(self.fam)
 
@@ -189,3 +306,40 @@ Example Notes without meaning
         per_species = self.hog_extractor.get_per_species_structure(self.fam)
         self.assertDictEqual(expected_HUMAN, per_species['HUMAN'])
         self.assertDictEqual(expected_MOUSE, per_species['MOUSE'])
+
+    @unittest.skip  # newick string is not invariant against ordering...
+    def test_topology(self):
+        expected = "(XENTR, ((MOUSE, (HUMAN, PANTR)Primates)Euarchontoglires, CANFA)Mammalia)Vertebrata;"
+        topology = self.hog_extractor.get_topology(self.fam)
+        self.assertEqual(expected, topology)
+
+    def test_xrefs(self):
+        exprected_of_human5 = {'id': "5", 'protId': "HUMAN5", 'geneId': "HUMANg5"}
+        xrefs = self.hog_extractor.get_xrefs(self.fam)
+        self.assertEqual(exprected_of_human5, xrefs[5])
+
+
+class WriterTest(unittest.TestCase):
+    def setUp(self):
+        self.dir = tempfile.mkdtemp()
+        self.extractor_mock = HOGVisExtractor(io.BytesIO(""))
+        self.extractor_mock.iter_families = MagicMock(return_value=(('(HUMAN,MOUSE)CANFA;',
+                                                                     {'HUMAN': [2, 4]}, {2: {'id': 2}}, 1)))
+
+    def test_write(self):
+        writer = Writer(self.dir, self.extractor_mock)
+        writer.dump_hogs()
+        expected_filename = os.path.join(self.dir, 'HOG{:06d}.html'.format(1))
+        self.assertTrue(os.path.exists(expected_filename))
+        with open(expected_filename) as fh:
+            data = fh.read()
+        self.assertIn('<title>HOG000001</title>', data)
+        self.assertIn('(HUMAN,MOUSE)CANFA;', data)
+
+    def tearDown(self):
+        shutil.rmtree(self.dir)
+
+if __name__ == "__main__":
+    ex = HOGVisExtractor('../Output/HierarchicalGroups.orthoxml')
+    wr = Writer('vis', ex)
+    wr.dump_hogs()
