e54aeca9a74c11438ade08f495fe922e0d0d3f9e
Adrian Altenhoff <adrian.altenhoff@inf.ethz.ch>
2015-11-27 17:49:06 +0100

    adding licence to familyanalyzer
    Former-commit-id: ccacad36ca39cfbb0cc182362eedb5258f4a08ef

--- setup.py
+++ setup.py
@@ -1,13 +1,32 @@
 from setuptools import setup, find_packages
+import os
 
 name = 'familyanalyzer'
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name=name,
     version='0.5.0',
     author='Adrian Altenhoff',
     author_email='adrian.altenhoff@inf.ethz.ch',
-    description='todoc',
+    description='A tool to analyse gene family evolution from orthoxml',
+    long_description=read('README.rst'),
+    license='MIT',
+    classifiers = [
+         'Development Status :: 3 - Alpha',
+         'Intended Audience :: Developers',
+         'Intended Audience :: Science/Research',
+         'Topic :: Scientific/Engineering :: Bio-Informatics',
+         'License :: OSI Approved :: MIT licence',
+         'Programming Language :: Python :: 2',
+         'Programming Language :: Python :: 2.7',
+         'Programming Language :: Python :: 3',
+         'Programming Language :: Python :: 3.3',
+         'Programming Language :: Python :: 3.4',
+         'Programming Language :: Python :: 3.5',
+         ],
     packages=find_packages(),
     install_requires=['lxml', 'progressbar-latest', 'future'],
     scripts=['bin/familyanalyzer']
